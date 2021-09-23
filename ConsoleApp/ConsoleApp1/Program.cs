d16629f5e5d5bc51a98b91555511833bfb6c7925
Aqil Safarov <aqil.safarov.2019@mail.ru>
2021-09-23 13:31:00 +0400

    console project

---
+++ ConsoleApp/ConsoleApp1/Program.cs
@@ -0,0 +1,71 @@
+﻿using finalproject;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Restoran menu = new Restoran("Bari Naxcivan");
+
+            Console.Write($"{menu.restoranNam} restorana xos gemisizniz!");
+            Console.WriteLine();
+            byte num;
+            do
+            {
+                byte reqem;
+
+                Console.WriteLine("Asagdaki reqemlerden isdifade ederek console proyekti isdifade ede bilersiniz"); 
+
+                Console.WriteLine("1.Add food");
+                Console.WriteLine("2.Order food");
+                Console.WriteLine("3.Show menu");
+                Console.WriteLine("0.Exit");  
+        
+                bool isTrueChoiise = byte.TryParse(Console.ReadLine(), out reqem); 
+                num = reqem;
+           
+                if (isTrueChoiise)
+                {
+                    switch (reqem)
+                    {
+                        case 1:
+                        {
+                                menu.AddOrder();
+                                break;
+                        }
+                        case 2:
+                        {
+                                menu.OrderFood();
+                                break;
+                        }
+                        case 3:
+                        {
+                                menu.ShowMenu();
+                                break;
+                        }
+                        default:
+                            {
+                                Console.WriteLine("Yalniz yuxarida verilen ededleri daxil ede bilersiz!!!");
+                                break;
+                            }
+                    }
+                  
+                } 
+                else
+                {
+                    num = 4;
+
+                    Console.WriteLine("Siz yalniz eded daxil ede bilersiniz");
+
+                }
+            } 
+            while (num!=0);  
+        }
+    }
+}
---
+++ ConsoleApp/ConsoleApp1/Restoran.cs
@@ -0,0 +1,134 @@
+﻿using finalproject;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Restoran
+    {
+        private string _restoranName;
+        public List<Menu> yemekler;
+
+
+        
+
+        public Restoran(string RestoranName)
+        {
+            this._restoranName = RestoranName;
+            yemekler = new List<Menu>();
+        }
+        public string restoranNam
+        {
+            get { return _restoranName; }
+        }
+
+
+        public void Addfood(string foodName,int quantity)
+        {
+            Menu m = new Menu();
+            m.Foodname = foodName;
+            m.Quantity = quantity;
+            yemekler.Add(m);
+        }
+        public void AddOrder()
+        {
+
+            Console.WriteLine("Yemeyin adini daxil edin:");
+            string a = Console.ReadLine();
+            Menu c = this.yemekler.Find(d => d.Foodname.ToLower() == a.ToLower());
+            if (c != null)
+            {
+                Console.WriteLine("Elave etmek istediyiniz porsiyani daxil edin:");
+                try
+                {
+                    int b = Int32.Parse(Console.ReadLine());
+                    c.Quantity += b;
+                    Console.WriteLine("Ugurla elave olundu");
+
+                }
+                catch (Exception)
+                {
+                    Console.WriteLine("Siz yalniz reqem daxil etmelisiniz");
+                   
+                }
+             
+            }
+            else
+            {
+                Console.WriteLine("Porsiyani daxil edin:");
+                try
+                {
+                    int b = Int32.Parse(Console.ReadLine());
+                    this.Addfood(a, b);
+                    Console.WriteLine("Ugurla elave olundu");
+
+                }
+                catch (Exception)
+                { 
+                    Console.WriteLine("Siz yalniz reqem daxil ede bilersiniz ");
+                }
+             
+            }
+        }
+       
+        public void OrderFood()
+        {
+            Console.WriteLine("Sifaris etmek istediyiniz yemeyin adini daxil edin:");
+            string d = Console.ReadLine();
+
+            Menu c = this.yemekler.Find(m => m.Foodname.ToLower() == d);
+            if (c != null)
+            {
+                if (c.Quantity != 0)
+                {
+                    Console.WriteLine("Yoxlanilir...");
+                    if (d != null)
+                    {
+                        Console.WriteLine("Yemek porsiyonunu daxil edin");
+                        int n = Convert.ToInt32(Console.ReadLine());
+                        if (c.Quantity >= n)
+                        {
+                            c.Quantity -= n;
+                            Console.WriteLine("Sifarisiniz qebul edildi");
+                            Console.WriteLine($"Qalan yemek sayi { c.Quantity}");
+                        }
+                        else
+                        {
+                            Console.WriteLine("Sifaris etdiyiniz yemekden kifayet qeder yoxdur");
+                        }
+                    }
+                    else
+                    {
+                        Console.WriteLine("Sifaris etmek isdediyiniz yemek artiq bitib"); 
+                    }
+                }
+                else
+                {
+                    Console.WriteLine("Sifariz etmek isdediyiniz yemek artiq  bitib");
+                }
+            }
+            else
+            {
+                Console.WriteLine("Bu yemek bizim menyumuzda yoxdur");
+            }
+        }
+        public void ShowMenu()
+        {
+            if (yemekler.Count == 0)
+            {
+                Console.WriteLine("Menyuya yemek elave olunmayib ");
+            }
+            else
+            {
+                foreach (var item in yemekler)
+                {
+                    Console.WriteLine($"Yemeyin adi: {item.Foodname},Yemeyin porsiyonu: {item.Quantity}");
+                }
+            }
+        }
+
+    }
+}
